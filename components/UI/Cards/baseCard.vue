342dffe97ecac0e7172275d3115d36abbfffcc9e
saman1945 <saman.alaii10@gmail.com>
2023-02-22 13:46:25 +0330

    ---

---
+++ components/UI/Cards/baseCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="base-card-container">
+    <slot name="content"></slot>
+    img
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.base-card-container {
+    width: 100%;
+    height: 100%;
+    background-color: var(--white);
+    padding: 16px;
+    box-shadow: -2px 4px 10px rgba(0, 0, 0, 0.05);
+	border-radius: 20px;
+}
+</style>
--- components/pageComponents/Main/TheHome.vue
+++ components/pageComponents/Main/TheHome.vue
@@ -1,25 +1,8 @@
 <template>
-  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-50" src="https://loremflickr.com/cache/resized/1407_1317060807_f6984f1552_n_320_240_nofilter.jpg" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-50" src="https://loremflickr.com/cache/resized/1407_1317060807_f6984f1552_n_320_240_nofilter.jpg" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-50" src="https://loremflickr.com/cache/resized/65535_51962769189_0b06e1c887_320_240_g.jpg" alt="Third slide">
-    </div>
+  <div class="image-container">
+    div
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
+
 </template>
 
 <script>
@@ -28,6 +11,7 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.image-container {
+ }
 </style>
